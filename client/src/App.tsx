9330b087efeed52c804fa9c9f630bf09d3aae997
Alexander <lixo.mueller@gmail.com>
2023-07-15 21:41:12 -0600

    About 90% done with the nav bar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,28 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './Pages/Home';
+import About from './Pages/About';
+import Contact from './Pages/Contact';
+import Gallery from './Pages/Gallery';
+import Menu from './Pages/Menu';
+import Testimonials from "./Pages/Testimonials";
+import Nav from "./Components/Navbar/Nav";
 
 function App() {
 
   return (
     <>
-      
+
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/Contact" element={<Contact />} />
+          <Route path="/Gallery" element={<Gallery />} />
+          <Route path="/Menu" element={<Menu />} />
+          <Route path="/Testimonials" element={<Testimonials />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ client/src/Components/Navbar/Nav.tsx
@@ -0,0 +1,101 @@
+import Logo from '../../Images/DeeplightRestaurantLogo.png';
+import MenuToggle from '../../Images/MenuToggleIcon.png';
+import RemoveMenu from '../../Images/RemoveMenu.png'
+import {useState} from 'react';
+import {Link} from 'react-router-dom';
+
+const Nav = () => {
+
+    const [toggleMenu, setToggleMenu] = useState(false);
+
+    const navContainer = {
+        padding: '15px 20px',
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    }
+
+    const menuContainer = {
+        display: 'flex',
+        flexDirection: 'column',
+        padding: '15px 20px',
+        backgroundColor: '#f5f5f5',
+    }
+
+    const logo = {
+        maxWidth: '135px',
+
+    }
+
+    const grabAndRemoveMenu = {
+        backgroundColor: '#eb5058',
+        padding: '7.5px',
+        width: '25px',
+        height: '25px'
+    }
+
+    const button = { 
+        border: 'none'
+    }
+
+    const menuLink = {
+        padding: '8px 0px',
+        borderTop: '#808080 solid 1px',
+        textDecoration: 'none',
+        color: '#808080',
+        fontSize: '12px'
+    }
+
+    const lastMenuLink = {
+        padding: '8px 0px 0px 0px',
+        borderTop: '#808080 solid 1px',
+        textDecoration: 'none',
+        color: '#808080',
+        fontSize: '12px'
+    }
+
+    const handleButtonClick = () => {
+        if (toggleMenu) {
+            setToggleMenu(false)
+        } else {
+            setToggleMenu(true);
+        }
+    }
+
+    return (
+        <>
+            <div style={navContainer}>
+                <img style={logo} src={Logo}></img>
+            {
+                toggleMenu == false ?
+                <>
+                    <button style={button} onClick={(e) => {e.preventDefault(); handleButtonClick();}}>
+                        <img style={grabAndRemoveMenu} src={MenuToggle}></img>
+                    </button>
+                </>
+
+                :
+
+                <>
+                    <button style={button} onClick={(e) => {e.preventDefault(); handleButtonClick();}}>
+                        <img style={grabAndRemoveMenu} src={RemoveMenu}></img>
+                    </button>
+                </>
+            }
+            </div>
+            {
+                toggleMenu == true &&
+                <>
+                    <div style={menuContainer}>
+                            <Link style={menuLink} to="/" onClick={(e) => {e.preventDefault(); handleButtonClick();}}>Home</Link>
+                            <Link style={menuLink}  to="/About" onClick={(e) => {e.preventDefault(); handleButtonClick();}}>About</Link>
+                            <Link style={menuLink}  to="/Gallery" onClick={(e) => {e.preventDefault(); handleButtonClick();}}>Gallery</Link>
+                            <Link style={menuLink} to="/Testimonials" onClick={(e) => {e.preventDefault(); handleButtonClick();}}>Testimonials</Link>
+                            <Link style={lastMenuLink} to="/Contact" onClick={(e) => {e.preventDefault(); handleButtonClick();}}>Contact</Link>
+                    </div>
+                </>
+            }
+        </>
+    );
+}
+export default Nav;
