a335840719e8a0b2e7d5f770c5ce9d85ca363478
Alexander <lixo.mueller@gmail.com>
2023-07-23 17:54:18 -0600

    last updated 07.23.2023

--- client/src/Pages/Home.tsx
+++ client/src/Pages/Home.tsx
@@ -6,6 +6,8 @@ const Home = () =>  {
 
     return (
         <main>
+            {
+                /*
             <section className='parallax'>
                 <div className='parallax-container'>
                     <div className='parallax-container-content'>
@@ -29,6 +31,31 @@ const Home = () =>  {
                 <p>A Few Words About Our Restaurant</p>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam. quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis.</p>
                 <button>Read More</button>
+            </section>
+            */
+            }
+            <section className='main-containers'>
+
+            </section>
+
+            <section className='main-containers-1'>
+                
+            </section>
+
+            <section className='main-containers'>
+                
+            </section>
+            
+            <section className='main-containers-1'>
+                
+            </section>
+            
+            <section className='main-containers'>
+                
+            </section>
+
+            <section className='main-containers-1'>
+                
             </section>
 
         </main>
